05c4f5241332487e56483961c1b69d07dcf2e214
chrisbuerginrogers <crogers@tufts.edu>
2024-09-26 09:31:57 -0400

    Update Simple Parallel.py

--- SPIKE3 examples/Simple Parallel.py
+++ SPIKE3 examples/Simple Parallel.py
@@ -1,22 +1,22 @@
 import runloop
 import motor
 from hub import port
-import display
+from hub import light_matrix
 
-p1,p2 = port.E,port.F
+p1,p2 = port.C,port.F
 
 async def motor_stack():
     degrees = 180
     speed = 1000
-    await motor.motor_move_by_degrees(p1, degrees, speed)
-    await motor.motor_move_by_degrees(p2, degrees, speed,motor.MOTOR_END_STATE_FLOAT)
-    await motor.motor_move_by_degrees(p1, -1 * degrees, speed,motor.MOTOR_END_STATE_FLOAT)
+    await motor.run_for_degrees(p1, degrees, speed)
+    await motor.run_for_degrees(p1, degrees, speed, stop = motor.SMART_COAST)
+    await motor.run_for_degrees(p1, -1 * degrees, speed, stop = motor.SMART_BRAKE)
     
 async def hub_pixel_stack():
-    display.display_set_pixel(1,1, 100)
-    await util.wait_for_time(2000)
-    display.display_clear()
-    await util.wait_for_time(1000)
-    display.display_set_pixel(2,2, 100)
+    light_matrix.set_pixel(1,1, 100)
+    await runloop.sleep_ms(20000)
+    light_matrix.clear()
+    await runloop.sleep_ms(10000)
+    display_set_pixel(2,2, 100)
     
-util.run(motor_stack(), hub_pixel_stack())
+runloop.run(motor_stack(), hub_pixel_stack())
